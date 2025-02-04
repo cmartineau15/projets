952562fa4b09e7049849bc4a0bf0e5d7d6280c20
koffi-eyes <koffi.adjai@epitech.digital>
2025-02-04 16:05:29 +0100

    Add files via upload

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Chat from './pages/Chat';
-import About from './pages/About';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import UserConditions from './pages/UserConditions';
 import CookiesPolicy from './pages/CookiesPolicy';
+import Chat2 from './pages/Chat_2';
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/chat" element={<Chat />} />
-        <Route path="/About" element={<About />} />
+        <Route path="/Chat_2" element={<Chat2 />} />
         <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
         <Route path="/UserConditions" element={<UserConditions />} />
         <Route path="/CookiesPolicy" element={<CookiesPolicy />} />
